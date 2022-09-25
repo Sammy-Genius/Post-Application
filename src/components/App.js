e33098e9ace661a66e8d035792550bb013f2ee12
sammy-genius <mrsammadeit@gmail.com>
2022-09-25 19:51:37 +0200

    modal for writing post

--- src/components/App.js
+++ src/components/App.js
@@ -14,9 +14,11 @@ function App() {
     <div className="w-screen h-screen bg-[#fad390] flexbox relative">
       <div className="w-[90%] md:w-[30%] h-[90%] bg-slate-50 border-radius z-10 relative">
         <Modal modal = { modal } openModal = {openModal}/>
-        <div className="absolute w-[60px] h-[60px] bg-[#e55039] right-[5%] bottom-[5%] rounded-full flexbox">
-          <span className="text-white text-2xl" onClick={ openModal }><FontAwesomeIcon icon={ faFeather} /></span>
-        </div>
+        { modal ? '' : 
+          <div className="absolute w-[60px] h-[60px] bg-[#e55039] right-[5%] bottom-[5%] rounded-full flexbox">
+            <span className="text-white text-2xl" onClick={ openModal }><FontAwesomeIcon icon={ faFeather} /></span>
+          </div> 
+        }
       </div>
       <div className="absolute w-full h-full bg-[#e55039] shape-one"></div>
     </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,19 @@ const Modal = ({modal, openModal}) => {
         <>
             { modal && (
             <div className="w-[100%] h-[100%] absolute overlay border-radius flexbox z-20">
-                <div className="w-[80%] h-[80%] bg-slate-50 border-radius"></div>
+                <div className="w-[80%] h-[80%] bg-slate-50 border-radius relative">
+                    <div className="w-[90%] h-[30%] my-[10%] mx-auto">
+                        <div className="title-box">
+                            <input type="text" placeholder="title" className='input' required />
+                        </div>
+                        <div className="description-box">
+                            <textarea className="text-area" placeholder="what's on your mind..."></textarea>
+                        </div>
+                    </div>
+                    <div className="absolute right-[5%] bottom-[5%] w-[70px] h-[40px] bg-[#e55039] curved flexbox" onClick = {openModal}>
+                        <p className="text-white">Post</p>
+                    </div>
+                </div>
             </div>
             )}
         </>
