7c8733f1cf3ee09b52b8e1821e71abddf1950224
sammy-genius <mrsammadeit@gmail.com>
2022-09-25 21:57:19 +0200

    logic for displaying posts done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,57 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFeather } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faFeather, faTrash } from "@fortawesome/free-solid-svg-icons";
 import Modal from "./Modal";
 
 function App() {
 
   const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [postList, setPostList] = useState([]);
 
   const openModal = () => {
     setModal(!modal);
   }
+
+  const postKleet = () => {
+    setPostList([...postList, {title: title, description: description, id: postList.length}]);
+    setTitle('');
+    setDescription('');
+    setModal(!modal);
+  }
+
   return (
     <div className="w-screen h-screen bg-[#fad390] flexbox relative">
       <div className="w-[90%] md:w-[30%] h-[90%] bg-slate-50 border-radius z-10 relative">
-        <Modal modal = { modal } openModal = {openModal}/>
+        <div className="w-full h-[15%] flexbox border-radius">
+          <h1 className="text-4xl font-black text-[#e55039] voyage">KleetApp</h1>
+        </div>
+        <div className="w-full h-auto mt-[30px]">
+          {postList.map((post) => {
+            return <div className="w-[90%] min-h-[60px] my-10 mx-auto flex flex-col items-center lines">
+              <div className="w-full h-full">
+                <h2 className="text-[#e55039] font-black voyage">{post.title}</h2>
+                <p className="mt-[10px] line-height">{post.description}</p>
+              </div>
+              <div className="w-[20%] h-full mt-[5px] ml-auto">
+                <span>
+                  <FontAwesomeIcon icon={ faEdit }/>
+                </span>
+                <span className="text-[#e55039] ml-[15px]">
+                  <FontAwesomeIcon icon={ faTrash }/>
+                </span>
+              </div>
+            </div>
+          })}
+        </div>
+        <Modal
+         modal = { modal } 
+         openModal = { openModal } 
+         setTitle = { setTitle } 
+         setDescription = { setDescription } 
+         postKleet = { postKleet } />
+    
         { modal ? '' : 
           <div className="absolute w-[60px] h-[60px] bg-[#e55039] right-[5%] bottom-[5%] rounded-full flexbox">
             <span className="text-white text-2xl" onClick={ openModal }><FontAwesomeIcon icon={ faFeather} /></span>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,22 @@
-const Modal = ({modal, openModal}) => {
+import { useState } from "react";
+
+const Modal = ({modal, setTitle, setDescription, postKleet}) => {
+
     return ( 
         <>
             { modal && (
-            <div className="w-[100%] h-[100%] absolute overlay border-radius flexbox z-20">
+            <div className="w-[100%] h-[100%] absolute top-0 leftt-0 overlay border-radius flexbox z-20">
                 <div className="w-[80%] h-[80%] bg-slate-50 border-radius relative">
                     <div className="w-[90%] h-[30%] my-[10%] mx-auto">
                         <div className="title-box">
-                            <input type="text" placeholder="title" className='input' required />
+                            <input type="text" placeholder="title" className='input' required onChange={e => setTitle(e.target.value)}/>
                         </div>
                         <div className="description-box">
-                            <textarea className="text-area" placeholder="what's on your mind..."></textarea>
+                            <textarea className="text-area" placeholder="what's on your mind..." onChange={e => setDescription(e.target.value)}></textarea>
                         </div>
                     </div>
-                    <div className="absolute right-[5%] bottom-[5%] w-[70px] h-[40px] bg-[#e55039] curved flexbox" onClick = {openModal}>
-                        <p className="text-white">Post</p>
+                    <div className="absolute right-[5%] bottom-[5%] w-[70px] h-[40px] bg-[#e55039] curved flexbox" onClick = {postKleet}>
+                        <p className="text-white voyage font-black cursor-pointer">Kleet</p>
                     </div>
                 </div>
             </div>
